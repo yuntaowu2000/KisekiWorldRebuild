b41dbf378b17dab37345d5bfbdde7391769c0d24
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2021-04-29 08:32:52 -0700

    Update

---
+++ Assets/Scripts/MapSelector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MapSelector : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            SceneManager.LoadScene("Grancel");
+        } else if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            SceneManager.LoadScene("Crossbell");
+        } else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            SceneManager.LoadScene("Heimdallr");
+        }
+    }
+}
