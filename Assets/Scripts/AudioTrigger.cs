f779d441b312c9bdcb788bca8c28f0a39d867ab4
yuntaowu2000 <49249544+yuntaowu2000@users.noreply.github.com>
2021-12-29 21:45:18 -0800

    finished new crossbell

---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    [SerializeField] AudioClip audioClip = null;
+    AudioSource audioSource = null;
+
+    private float fadeSpeed = 0.5f;
+
+    private void Start() {
+        audioSource = FindObjectOfType<AudioSource>();
+    }
+
+    IEnumerator PlayAudio() {
+        if (audioSource.clip != null) {
+            while (!float.Equals(audioSource.volume, 0.0f)) {
+                audioSource.volume -= fadeSpeed * Time.deltaTime;
+                yield return null;
+            }
+        }
+        audioSource.Stop();
+        audioSource.volume = 1.0f;
+        audioSource.clip = audioClip;
+        audioSource.Play();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        // if (other.gameObject.layer != LayerMask.NameToLayer("User")) return;
+        if (other.gameObject.tag != "Player" 
+        || audioClip == null
+        || (audioSource.clip != null && audioSource.clip.name == audioClip.name)) return;
+
+        StartCoroutine(PlayAudio());
+    }
+
+    // private void OnTriggerStay(Collider other) {
+    //     if (other.gameObject.tag != "Player" 
+    //     || audioClip != null
+    //     || audioSource.clip.name == audioClip.name) return;
+    // }
+
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using StarterAssets;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField] Transform center;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.rKey.isPressed) {
+            GameObject player = GameObject.FindWithTag("Player");
+            // player.GetComponent<Collider>().enabled = false;
+            player.GetComponent<ThirdPersonController>().enabled = false;
+            player.transform.position = center.position;
+            player.transform.rotation = center.rotation;
+            // player.GetComponent<Collider>().enabled = true;
+            player.GetComponent<ThirdPersonController>().enabled = true;
+        }
+    }
+}
